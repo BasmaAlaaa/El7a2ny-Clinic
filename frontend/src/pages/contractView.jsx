492553c9d1b9f318606b242ad52f481da9a3fe8d
Basma Alaa Mahmoud <121583270+basmaAlaaElSayed@users.noreply.github.com>
2023-11-07 01:48:57 +0200

    Basmaa_Backup_lia

--- frontend/src/pages/contractView.jsx
+++ frontend/src/pages/contractView.jsx
@@ -13,6 +13,7 @@ function ContractView () {
   const [contractInfo, setContractInfo] = useState(null);
 
   useEffect(() => {
+
     const fetchContract = async () => {
       try {
         const response = await axios.get(`http://localhost:4000/Doctor/viewContract/${username}`);
