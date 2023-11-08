0a85a9df89f7116ba5abc339000b0208c7e0e6dd
samaelharras <111366821+samaelharras@users.noreply.github.com>
2023-11-08 14:05:03 +0200

    view health records patient done

--- frontend/src/pages/patientView.jsx
+++ frontend/src/pages/patientView.jsx
@@ -4,11 +4,23 @@ import DoctorsList from "../components/DoctorsList";
 import PrescriptionsList from "../components/PrescriptionsList";
 import MainBtn from "../components/Button";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import TableHealthRecords from "../components/TableHealthRecords";
+
 
 
 function PatientView(){
   const navigate = useNavigate();
   const {username} = useParams();
+  const [healthRecord, setHealthRecord] = useState([]);
+
+  let tHead = ['Date', 'Description', 'Diagnosis', 'Medication'];
+  useEffect(() => {
+    const response = axios.get(`http://localhost:4000/Patient/viewHealthRecords/${username}`)
+    .then(res =>setHealthRecord(res.data.healthRecords)).catch(err => console.log(err))
+      }, [])
+  console.log(healthRecord);
 return (
     <div>
     <NavBarPatient username={username}/>
@@ -47,6 +59,8 @@ return (
           />
           </div>
     <DoctorsList/>
+    <h1>Health Records </h1>
+         <TableHealthRecords tHead={tHead} data={healthRecord} /> 
 
     </div>
 )
