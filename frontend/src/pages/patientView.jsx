e92f5c95adba23df6ed9daefb0452490d12ab138
Mireille Bishay <110732750+mireillebishay@users.noreply.github.com>
2023-11-09 20:20:13 +0200

    Update patientView.jsx

--- frontend/src/pages/patientView.jsx
+++ frontend/src/pages/patientView.jsx
@@ -8,67 +8,73 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import TableHealthRecords from "../components/TableHealthRecords";
 
-
-
-function PatientView(){
+function PatientView() {
   const navigate = useNavigate();
-  const {username} = useParams();
+  const { username } = useParams();
   const [healthRecord, setHealthRecord] = useState([]);
 
+
   let tHead = ['Date', 'Description', 'Diagnosis', 'Medication'];
+
   useEffect(() => {
-    const response = axios.get(`http://localhost:4000/Patient/viewHealthRecords/${username}`)
-    .then(res =>setHealthRecord(res.data.healthRecords)).catch(err => console.log(err))
-      }, [])
-  console.log(healthRecord);
-return (
+    axios.get(`http://localhost:4000/Patient/viewHealthRecords/${username}`)
+      .then(res => setHealthRecord(res.data.healthRecords))
+      .catch(err => console.log(err));
+  }, [username]);
+
+  return (
     <div>
-    <NavBarPatient username={username}/>
-    <div>
-            <MainBtn
-              txt="View All Appointments"
-              style="green-btn"
-              action={() => navigate(`/appointmentsList/${username}`)}
-              key="navBtn"
-            />
-            <MainBtn
-              txt="View Health Packages"
-              style="green-btn"
-              action={() => navigate(`/healthPackagesList/${username}`)}
-              key="navBtn"
-            />
-            
-            <MainBtn
-              txt="View All Registered Family Members"
-              style="green-btn"
-              action={() => navigate(`/familyMembersList/${username}`)}
-              key="navBtn"
-            />
-            
-            <MainBtn
-            txt="View All of my Prescriptions"
-            style="green-btn"
-            action={() => navigate(`/prescriptionsList/${username}`)}
-            key="navBtn"
-          />
-            <MainBtn
-            txt="Add Family Member"
-            style="green-btn"
-            action={() => navigate(`/addFamilyMember/${username}`)}
-            key="navBtn"
-          />
-          <MainBtn
-            txt="Add Medical History Document"
-            style="green-btn"
-            action={() => navigate(`/addMedicalHistoryDocument/${username}`)}
-            key="navBtn"
-          />
-          </div>
-    <DoctorsList/>
-    <h1>Health Records </h1>
-         <TableHealthRecords tHead={tHead} data={healthRecord} /> 
+      <NavBarPatient username={username} />
+      <div>
+        <MainBtn
+          txt="View All Appointments"
+          style="green-btn"
+          action={() => navigate(`/appointmentsList/${username}`)}
+          key="navBtn"
+        />
+        <MainBtn
+          txt="View Health Packages"
+          style="green-btn"
+          action={() => navigate(`/healthPackagesList/${username}`)}
+          key="navBtn"
+        />
+        <MainBtn
+          txt="View All Registered Family Members"
+          style="green-btn"
+          action={() => navigate(`/familyMembersList/${username}`)}
+          key="navBtn"
+        />
+        <MainBtn
+          txt="View All of my Prescriptions"
+          style="green-btn"
+          action={() => navigate(`/prescriptionsList/${username}`)}
+          key="navBtn"
+        />
+        <MainBtn
+          txt="Add Family Member"
+          style="green-btn"
+          action={() => navigate(`/addFamilyMember/${username}`)}
+          key="navBtn"
+        />
+        <MainBtn
+          txt="Add Medical History Document"
+          style="green-btn"
+          action={() => navigate(`/addMedicalHistoryDocument/${username}`)}
+          key="navBtn"
+        />
+        <MainBtn
+          txt="View Medical History Documents"
+          style="green-btn"
+          action={() => navigate(`/viewMedicalHistoryDocuments/${username}`)}
+          key="navBtn"
+        />
+      </div>
+      <DoctorsList />
+      <h1>Health Records</h1>
+      <TableHealthRecords tHead={tHead} data={healthRecord} />
 
     </div>
-)
+  );
 }
+
 export default PatientView;
